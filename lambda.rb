a70023f6d3bece662898ba1a42a014fd3cb0c6ac
Kugayama Nana <nonamea774@gmail.com>
2019-05-07 15:15:40 +0900

    remove last line newline

--- lambda.rb
+++ lambda.rb
@@ -11,7 +11,7 @@ def handler(event:, context:)
   to = project + '/'
   to +=  "#{today.strftime('%Y%%2F%m')}"
   body = (today.beginning_of_month..today.end_of_month).map {|d| d.strftime('[%Y/%m/%d]') }.each_slice(7).map{|ds| ds.join(' ') }.join("\n")
-  body += "\n\n#{today.strftime('[%Y年]')} #{today.strftime('[%1m月]')}\n\n[月]\n"
+  body += "\n\n#{today.strftime('[%Y年]')} #{today.strftime('[%1m月]')}\n\n[月]"
   to += "?body=#{URI.encode(body)}"
   { location: to }
 end
